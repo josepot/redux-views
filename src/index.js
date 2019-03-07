38682593c4b347a3f7981eb2cf52f3c120a8d472
Josep M Sobrepere <josepot@gmail.com>
2019-03-07 02:32:38 +0100

    Improved API: v0.1.0

--- src/index.js
+++ src/index.js
@@ -1,71 +1,127 @@
-const alwaysZero = () => 0
-const emptyArr = []
+const getNewKeySelector = fns => (...args) =>
+  fns.map(fn => encodeURIComponent(fn(...args))).join('/')
+
+const combinedKeySelectors = {}
+const getCombinedKeySelector = keySelectors => {
+  const len = keySelectors.length
+  if (!combinedKeySelectors[len]) combinedKeySelectors[len] = []
+
+  const entry = combinedKeySelectors[len].find(([candidates]) =>
+    candidates.every(c => keySelectors.some(k => k === c))
+  )
+
+  if (entry) return entry[1]
+
+  const fn = getNewKeySelector(keySelectors)
+  combinedKeySelectors[len].push([keySelectors, fn])
+  return fn
+}
+
+const getKeySelector = dependencies => {
+  const keySelectors = dependencies.map(x => x.keySelector).filter(Boolean)
+
+  if (keySelectors.length === 0) return null
+  return keySelectors.length === 1
+    ? keySelectors[0]
+    : getCombinedKeySelector(keySelectors)
+}
+
+const getKeySelectorEntries = (dependencies, keySelector, compute) => {
+  const usableDependencies = dependencies.filter(
+    d => typeof d.use === 'function'
+  )
+
+  const result = [[keySelector, [compute]]]
+  const kSelectorsMap = new WeakMap([[keySelector, 0]])
+
+  usableDependencies.forEach(d => {
+    d.keySelectorEntries.forEach(([kSelector, selectors]) => {
+      const idx = kSelectorsMap.get(kSelector)
+      if (idx !== undefined) {
+        result[idx][1].push(...selectors)
+      } else {
+        kSelectorsMap.set(kSelector, result.length)
+        result.push([kSelector, [...selectors]])
+      }
+    })
+  })
+  return result
+}
 
 class Selector {
-  constructor(dependencies, computeFn, keySelector = alwaysZero) {
+  constructor(dependencies, computeFn) {
     this.dependencies = dependencies
     this.computeFn = computeFn
-    this.keySelector = keySelector
-
-    this.data = new Map()
-    this.usages = new Map()
+    this.nComputations = 0
     this.compute = this.compute.bind(this)
     this.compute.__ref__ = this
-
-    this.usableDependencies = dependencies.filter(
-      d => typeof d.use === 'function'
-    )
-
-    this.keySelectorEntries = [[keySelector, [this.compute]]]
-    const kSelectorsMap = new WeakMap([[keySelector, 0]])
-
-    this.usableDependencies.forEach(d => {
-      d.__ref__.getKeySelectorEntries().forEach(([kSelector, selectors]) => {
-        const idx = kSelectorsMap.get(kSelector)
-        if (idx !== undefined) {
-          this.keySelectorEntries[idx][1].push(...selectors)
-        } else {
-          kSelectorsMap.set(kSelector, this.keySelectorEntries.length)
-          this.keySelectorEntries.push([kSelector, [...selectors]])
-        }
-      })
-    })
-
-    this.compute.use = () => {
-      let prevKeys = new Array(this.keySelectorEntries.length)
-      let prevFns = new Array(this.keySelectorEntries.length)
-      return (...args) => {
-        const keys = this.keySelectorEntries.map(([ks]) => ks(...args))
-        keys.forEach((key, idx) => {
-          if (key !== prevKeys[idx]) {
-            prevKeys[idx] = key
-            prevFns[idx] = () => {
-              this.keySelectorEntries[idx][1].forEach(s => s.__ref__.inc(key))
-              return () => {
-                this.keySelectorEntries[idx][1].forEach(s => s.__ref__.dec(key))
-              }
-            }
-          }
-        })
-        return prevFns
-      }
-    }
   }
 
   compute(...args) {
-    const key = this.keySelector(...args)
+    const cache = this.getCache(...args)
+    const [prevArgs, prevRes] = cache
     const computeFnArgs = this.dependencies.map(fn => fn(...args))
-    const [prevArgs, prevRes] = this.data.get(key) || emptyArr
     if (prevArgs && computeFnArgs.every((val, idx) => val === prevArgs[idx])) {
       return prevRes
     }
     const res = this.computeFn(...computeFnArgs)
-    this.data.set(key, [computeFnArgs, res])
+    this.nComputations++
+    cache[0] = computeFnArgs
+    cache[1] = res
     return res
   }
+}
 
-  getKeySelectorEntries() {
-    return this.keySelectorEntries
+const selfBind = (that, ...fns) =>
+  fns.forEach(fn => {
+    that[fn] = that[fn].bind(that)
+  })
+
+class InstanceSelector extends Selector {
+  constructor(dependencies, computeFn, keySelector, cache) {
+    super(dependencies, computeFn)
+    this.keySelector = keySelector
+    this.keySelectorEntries = getKeySelectorEntries(
+      dependencies,
+      this.keySelector,
+      this.compute
+    )
+    this.cache = cache || new Map()
+    this.usages = new Map()
+
+    selfBind(this, 'inc', 'dec', 'getCache', 'use')
+
+    this.compute.keySelector = keySelector
+    this.compute.keySelectorEntries = this.keySelectorEntries
+    this.compute.inc = this.inc
+    this.compute.dec = this.dec
+    this.compute.use = this.use
+  }
+
+  getCache(...args) {
+    const key = this.keySelector(...args)
+    if (!this.cache.has(key)) this.cache.set(key, new Array(2))
+    return this.cache.get(key)
+  }
+
+  use() {
+    let prevKeys = new Array(this.keySelectorEntries.length)
+    let prevFns = new Array(this.keySelectorEntries.length)
+    return (...args) => {
+      const keys = this.keySelectorEntries.map(([ks]) => ks(...args))
+      keys.forEach((key, idx) => {
+        if (key !== prevKeys[idx]) {
+          prevKeys[idx] = key
+          prevFns[idx] = () => {
+            this.keySelectorEntries[idx][1].forEach(s => s.inc(key))
+            return () => {
+              this.keySelectorEntries[idx][1].forEach(s => s.dec(key))
+            }
+          }
+        }
+      })
+      return prevFns
+    }
   }
 
   inc(key) {
@@ -76,21 +132,69 @@ class Selector {
   dec(key) {
     const count = this.usages.get(key)
     if (count === undefined) return
-    if (count === 1) return this.data.delete(key)
+    if (count === 1) this.cache.delete(key)
     this.usages.set(key, count - 1)
   }
 }
 
-export default (dependencies_, computeFn_, keySelector) => {
-  if (!computeFn_ && !keySelector && typeof dependencies_ === 'object') {
-    const dependencies = Object.values(dependencies_)
-    const keys = Object.keys(dependencies_)
-    const computeFn = (...values) => {
-      const res = Array.isArray(dependencies_) ? new Array(keys.length) : {}
-      values.forEach((val, idx) => (res[keys[idx]] = val))
-      return res
-    }
-    return new Selector(dependencies, computeFn, keySelector).compute
+class FlatSelector extends Selector {
+  constructor(dependencies, computeFn) {
+    super(dependencies, computeFn)
+    this.cache = new Array(2)
+    this.usages = 0
+    this.getCache = this.getCache.bind(this)
   }
-  return new Selector(dependencies_, computeFn_, keySelector).compute
+
+  getCache() {
+    return this.cache
+  }
+}
+
+const getDependencies = args =>
+  args.length === 1 && Array.isArray(args[0]) ? args[0] : args
+
+export const createSelector = (...args) => {
+  const [computeFn] = args.splice(-1)
+  const dependencies = getDependencies(args)
+  const keySelector = getKeySelector(dependencies)
+  const SelectorType = keySelector ? InstanceSelector : FlatSelector
+  return new SelectorType(dependencies, computeFn, keySelector).compute
+}
+
+export const createKeyedSelectorFactory = (...args) => {
+  const [computeFn] = args.splice(-1)
+  const dependencies = getDependencies(args)
+  const cache = new Map()
+  return selector => {
+    const keySelector = createKeySelector(selector)
+    return new InstanceSelector(
+      [...dependencies, keySelector],
+      computeFn,
+      keySelector,
+      cache
+    ).compute
+  }
+}
+
+export const createKeySelector = x => {
+  const keySelector = createSelector(
+    [s => s, (s, p) => p],
+    x
+  )
+  keySelector.keySelector = keySelector
+  return keySelector
+}
+
+export const createStructuredSelector = obj => {
+  const dependencies = Object.values(obj)
+  const keys = Object.keys(obj)
+  const compute = (...vals) => {
+    const res = {}
+    vals.forEach((val, idx) => (res[keys[idx]] = val))
+    return res
+  }
+  return createSelector(
+    dependencies,
+    compute
+  )
 }
--- test/test.spec.js
+++ test/test.spec.js
@@ -1,76 +1,211 @@
-import createSelector from '../src'
-const prop = key => state => state[key]
+import { add, prop } from 'ramda'
+import {
+  createSelector,
+  createKeySelector,
+  createKeyedSelectorFactory
+} from '../src'
 
 const state = {
-  messages: {
-    1: { id: 1, text: 'foo1' },
-    2: { id: 2, text: 'foo2' },
-    3: { id: 3, text: 'foo2' },
-    4: { id: 4, text: 'foo3' },
-    5: { id: 5, text: 'foo4' },
-    6: { id: 6, text: 'foo5' },
-    7: { id: 7, text: 'foo6' },
-    8: { id: 8, text: 'foo7' },
-    9: { id: 9, text: 'foo8' }
-  },
   users: {
-    foo: { id: 'foo', name: 'foo' },
-    bar: { id: 'bar', name: 'bar' }
-  },
-  usersMessages: {
-    foo: [1, 2, 3, 4, 5],
-    bar: [6, 7, 8, 9]
+    1: { id: 1, name: 'foo1' },
+    2: { id: 2, name: 'foo2' },
+    3: { id: 3, name: 'fooo3' },
+    4: { id: 4, name: 'foooo4' },
+    5: { id: 5, name: 'foo4' },
+    6: { id: 6, name: 'foo5' },
+    7: { id: 7, name: 'foo6' },
+    8: { id: 8, name: 'foo7' },
+    9: { id: 9, name: 'foo8' }
   }
 }
 
-const messagesSelector = prop('messages')
-const usersSelector = prop('users')
-const usersMessagesSelector = prop('usersMessages')
+const getUsers = prop('users')
 
-const getUserKey = (state, { userId }) => userId
+describe('createSelector', () => {
+  describe('computes dependant function', () => {
+    test('dependencies can be grouped in an Array', () => {
+      const selector = createSelector(
+        [prop('a'), prop('b')],
+        add
+      )
+      expect(selector({ a: 3, b: 4 })).toEqual(7)
+    })
 
-const messageKeysPerUser = createSelector(
-  [usersMessagesSelector, getUserKey],
-  (usersMessages, key) => usersMessages[key],
-  getUserKey
-)
+    test('dependencies do not have to be grouped in an Array', () => {
+      const selector = createSelector(
+        prop('a'),
+        prop('b'),
+        add
+      )
+      expect(selector({ a: 3, b: 4 })).toEqual(7)
+    })
+  })
+})
 
-const nMessagesPerUser = createSelector(
-  [messageKeysPerUser],
-  x => (x ? x.length : 0),
-  getUserKey
-)
+describe('createKeyedSelectorFactory', () => {
+  test('returns a factory of keyed-selectors with a shared cache', () => {
+    const getUserFactory = createKeyedSelectorFactory(
+      getUsers,
+      (users, key) => users[key]
+    )
 
-describe('test', () => {
-  test('test', () => {
-    const getFooSubscriptions = nMessagesPerUser.use()
-    const getFooSubscriptions2 = nMessagesPerUser.use()
-    const getBarSubscriptions = nMessagesPerUser.use()
+    const getPropsFrom = jest.fn((s, { from }) => from)
+    const getPropsTo = jest.fn((s, { to }) => to)
+    const getUserFrom = getUserFactory(getPropsFrom)
+    const getUserTo = getUserFactory(getPropsTo)
 
-    const fooProps = { userId: 'foo' }
-    const fooFns = getFooSubscriptions(state, fooProps)
-    const fooFns2 = getFooSubscriptions2(state, fooProps)
-    const fooUnsubscriptions = fooFns.map(f => f())
-    const fooUnsubscriptions2 = fooFns2.map(f => f())
-    const fooMessages = nMessagesPerUser(state, fooProps)
-    expect(fooMessages).toEqual(5)
-    expect(nMessagesPerUser.__ref__.usages.get('foo')).toEqual(2)
+    expect(getUserFrom.__ref__.cache instanceof Map).toBe(true)
+    expect(getUserFrom.__ref__.cache.size).toBe(0)
+    expect(getUserTo.__ref__.cache).toBe(getUserFrom.__ref__.cache)
 
-    const barProps = { userId: 'bar' }
-    const barFns = getBarSubscriptions(state, barProps)
-    const barUnsubscriptions = barFns.map(f => f())
-    const barMessages = nMessagesPerUser(state, barProps)
-    expect(barMessages).toEqual(4)
+    expect(getPropsFrom.mock.calls.length).toBe(0)
+    expect(getUserFrom.__ref__.cache.size).toBe(0)
 
-    expect(nMessagesPerUser.__ref__.data.size).toEqual(2)
+    expect(getUserFrom(state, { from: 1 })).toBe(state.users[1])
+    expect(getUserFrom.__ref__.cache.size).toBe(1)
+    expect(getPropsFrom.mock.calls.length).toBe(1)
+    expect(getUserTo.__ref__.nComputations).toBe(0)
 
-    fooUnsubscriptions.forEach(fn => fn())
-    expect(nMessagesPerUser.__ref__.data.size).toEqual(2)
+    expect(getUserTo(state, { to: 1 })).toBe(state.users[1])
+    expect(getUserFrom.__ref__.cache.size).toBe(1)
+    expect(getUserTo.__ref__.nComputations).toBe(0)
+    expect(getPropsTo.mock.calls.length).toBe(1)
+    expect(getUserTo.__ref__.nComputations).toBe(0)
 
-    fooUnsubscriptions2.forEach(fn => fn())
-    expect(nMessagesPerUser.__ref__.data.size).toEqual(1)
+    expect(getUserTo(state, { to: 2 })).toBe(state.users[2])
+    expect(getUserFrom.__ref__.cache.size).toBe(2)
+    expect(getUserTo.__ref__.nComputations).toBe(1)
+    expect(getPropsTo.mock.calls.length).toBe(2)
+  })
+})
 
-    barUnsubscriptions.forEach(fn => fn())
-    expect(nMessagesPerUser.__ref__.data.size).toEqual(0)
+describe('keyed selectors', () => {
+  let getFromProp, getToProp, getUserFrom, getUserTo, joinNames, getJoinedNames
+
+  beforeEach(() => {
+    getFromProp = createKeySelector((state, { from }) => from)
+    getToProp = createKeySelector((state, { to }) => to)
+
+    getUserFrom = createSelector(
+      getFromProp,
+      getUsers,
+      prop
+    )
+    getUserTo = createSelector(
+      getToProp,
+      getUsers,
+      prop
+    )
+
+    joinNames = jest.fn(
+      ({ name: nameA }, { name: nameB }) => `${nameA}-${nameB}`
+    )
+
+    getJoinedNames = createSelector(
+      [getUserFrom, getUserTo],
+      joinNames
+    )
+  })
+
+  describe('cache and recomputations', () => {
+    test('it detects those selectors that its cache should be keyed', () => {
+      const joinedOneTwo = getJoinedNames(state, { from: 1, to: 2 })
+      expect(joinedOneTwo).toEqual('foo1-foo2')
+      expect(joinNames.mock.calls.length).toBe(1)
+
+      const joinedOneThree = getJoinedNames(state, { from: 1, to: 3 })
+      expect(joinedOneThree).toEqual('foo1-fooo3')
+      expect(joinNames.mock.calls.length).toBe(2)
+      expect(getUserTo.__ref__.nComputations).toBe(2)
+      expect(getUserTo.__ref__.cache.size).toBe(2)
+      expect(getUserFrom.__ref__.nComputations).toBe(1)
+      expect(getUserFrom.__ref__.cache.size).toBe(1)
+
+      const joinedOneTwoAgain = getJoinedNames(state, { from: 1, to: 2 })
+      expect(joinedOneTwoAgain).toBe(joinedOneTwo)
+      expect(joinNames.mock.calls.length).toBe(2)
+      expect(getUserTo.__ref__.nComputations).toBe(2)
+      expect(getUserTo.__ref__.cache.size).toBe(2)
+      expect(getUserFrom.__ref__.nComputations).toBe(1)
+      expect(getUserFrom.__ref__.cache.size).toBe(1)
+
+      const newState = {
+        ...state,
+        users: {
+          ...state.users,
+          newOne: {}
+        }
+      }
+
+      const joinedOneThreeAgain = getJoinedNames(newState, { from: 1, to: 3 })
+      expect(joinedOneThreeAgain).toBe(joinedOneThree)
+      expect(joinNames.mock.calls.length).toBe(2)
+      expect(getUserTo.__ref__.nComputations).toBe(3)
+      expect(getUserTo.__ref__.cache.size).toBe(2)
+      expect(getUserFrom.__ref__.nComputations).toBe(2)
+      expect(getUserFrom.__ref__.cache.size).toBe(1)
+      expect(getJoinedNames.__ref__.nComputations).toBe(2)
+      expect(getJoinedNames.__ref__.cache.size).toBe(2)
+    })
+  })
+
+  describe('usage: refCounts', () => {
+    test('it cleans the cache when all users unsubscribe', () => {
+      let s = state
+      let props
+
+      const getSubscriptionsOneTwo = getJoinedNames.use()
+      props = { from: 1, to: 2 }
+      const oneTwoSubscriptions = getSubscriptionsOneTwo(s, props)
+      const oneTwoUns = oneTwoSubscriptions.map(fn => fn())
+      getJoinedNames(s, props)
+
+      const getSubscriptionsOneThree = getJoinedNames.use()
+      props = { from: 1, to: 3 }
+      const oneThreeSubscriptions = getSubscriptionsOneThree(s, props)
+      const oneThreeUns = oneThreeSubscriptions.map(fn => fn())
+      getJoinedNames(s, props)
+
+      const getSubscriptionsTwoFour = getJoinedNames.use()
+      props = { from: 2, to: 4 }
+      const twoFourSubscriptions = getSubscriptionsTwoFour(s, props)
+      const twoFourUns = twoFourSubscriptions.map(fn => fn())
+      getJoinedNames(s, props)
+
+      s = {
+        ...state,
+        users: {
+          ...state.users,
+          newOne: {}
+        }
+      }
+
+      const oneTwoSubscriptionsAgain = getSubscriptionsOneTwo(s, props)
+      getJoinedNames(s, props)
+
+      expect(oneTwoSubscriptionsAgain.length).toBe(oneTwoSubscriptions.length)
+      oneTwoSubscriptionsAgain.forEach((s, idx) => {
+        expect(s).toBe(oneTwoSubscriptions[idx])
+      })
+
+      expect(getJoinedNames.__ref__.cache.size).toBe(3)
+      expect(getUserTo.__ref__.cache.size).toBe(3)
+      expect(getUserFrom.__ref__.cache.size).toBe(2)
+
+      oneTwoUns.forEach(fn => fn())
+      expect(getJoinedNames.__ref__.cache.size).toBe(2)
+      expect(getUserTo.__ref__.cache.size).toBe(2)
+      expect(getUserFrom.__ref__.cache.size).toBe(2)
+
+      oneThreeUns.forEach(fn => fn())
+      expect(getJoinedNames.__ref__.cache.size).toBe(1)
+      expect(getUserTo.__ref__.cache.size).toBe(1)
+      expect(getUserFrom.__ref__.cache.size).toBe(1)
+
+      twoFourUns.forEach(fn => fn())
+      expect(getJoinedNames.__ref__.cache.size).toBe(0)
+      expect(getUserTo.__ref__.cache.size).toBe(0)
+      expect(getUserFrom.__ref__.cache.size).toBe(0)
+    })
   })
 })
